c7836043650c3ca0805384438fa5be57fd01a4dc
patrickpowers848@gmail.com <patrickpowers848@gmail.com>
2017-06-19 20:38:59 -0400

    updating shell

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
-var port = 3000;
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
 var app = express();
 var path = require('path');
+
+
 
 app.use(express.static('./public'));
 
@@ -11,6 +14,19 @@ app.get("/", function(req, res){
 
 app.get("/home", function(req, res){
 	res.sendFile(path.resolve(__dirname, './public/home.html'));
+});
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/music-forward";
+
+mongoose.connect(db, function(error) {
+  // Log any errors connecting with mongoose
+  if (error) {
+    console.log(error);
+  }
+  // Or log a success message
+  else {
+    console.log("mongoose connection is successful");
+  }
 });
 
 app.listen(port, function() {
